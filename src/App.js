89cce0c39cd0c68e548f888b95548606797716c3
Shareena Begum <shareena.fsp@gmail.com>
2021-07-27 14:55:33 +0200

    seperate component is created as todoitem  as phase-2 rreact todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import TodoItem from "./TodoItem.js"
 
 
 function App() {
   return (
     <div>
-      <input type="checkbox"/>
-      <p>Placeholder text here</p>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+
     </div>
   );
 }
