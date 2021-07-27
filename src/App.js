02d8266b3c17dc8bbabae5b0c20ab2195586fd87
Shareena Begum <shareena.fsp@gmail.com>
2021-07-27 16:04:28 +0200

    phase-4 react... class and component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import { render } from "@testing-library/react"
 import React from "react"
 import TodoItem from "./TodoItem.js"
 import todosData from "./todosData.js"
 
 
-function App() {
-const todoItems = todosData.map(item => <TodoItem key={item.id} item={item} />)
-
+class App extends React.Component {
+  constructor(){
+    super()
+      this.state = {
+      todos: todosData
+    }
+  }
+ 
+  render(){
+    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item} />)
   return (
     <div className="todo-list">
       {todoItems}
     </div>
-  );
+    )
+  }
 }
 
 export default App;
